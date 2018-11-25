c96262692104e25bebbbcafbad9f2dcc3f063422
Nevin Chen <cnevinchen@gmail.com>
2018-11-25 06:07:06 +0100

    remove unused

--- app/src/test/java/com/example/nechen/calculator/CalculatorTest.kt
+++ app/src/test/java/com/example/nechen/calculator/CalculatorTest.kt
@@ -29,25 +29,25 @@ class CalculatorTest {
             }
 
             // first , op , output
-            test("3", "3")  //  3, none, 0
-            test("2", "32")  // 32,none, 0
-            test("1", "321")  // 321,none, 0
-            test("PLUS", "321")  // 321,plus, 321 <-
-            test("PLUS", "321")  // 321,plus, 321
-            test("PLUS", "321")  // 321,plus, 321
-            test("MINUS", "321")  // 321,minus, 321 <-
-            test("2", "2")  // 2,minus, 321
-            test("1", "21")  // 21,minus, 321
-            test("PLUS", "300")  // 321,div, 300 <-
-            test("3", "3")  // 3,div, 300
-            test("0", "30")  // 30,div, 300
-            test("EQUAL", "330")  // 10,evl, 10  <-
-            test("MULTIPLY", "330")  // 10,evl, 10  <-
-            test("1", "1")  // 10,evl, 10  <-
-            test("0", "10")  // 10,evl, 10  <-
-            test("DIVIDE", "3300")  // 10,evl, 10  <-
-            test("0", "0")  // 10,evl, 10  <-
-            test("EQUAL", NOT_A_NUMBER)  // 10,evl, 10  <-
+            test("3", "3")
+            test("2", "32")
+            test("1", "321")
+            test("PLUS", "321")
+            test("PLUS", "321")
+            test("PLUS", "321")
+            test("MINUS", "321")
+            test("2", "2")
+            test("1", "21")
+            test("PLUS", "300")
+            test("3", "3")
+            test("0", "30")
+            test("EQUAL", "330")
+            test("MULTIPLY", "330")
+            test("1", "1")
+            test("0", "10")
+            test("DIVIDE", "3300")
+            test("0", "0")
+            test("EQUAL", NOT_A_NUMBER)
 
         }
 
